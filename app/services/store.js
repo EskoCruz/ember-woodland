9c969057da48da684bf26cf8fb77520051699cb2
Esko Cruzeta <eskocruz@users.noreply.github.com>
2016-05-24 12:41:22 -0700

    update "store" service to source order data from "orders" array

--- app/services/store.js
+++ app/services/store.js
@@ -10,18 +10,19 @@ const products = [
 	Product.create({ title: 'First-Aid Kit', price: 3, description: 'This First Aid Kit is an extensively stocked first aid kit with everything you need for peace of mind.' })
 ];
 
+const orders = [
+	Order.create({ id: '1234', name: 'Blaise Blobfish',
+		items: [
+			LineItem.create({product: products[0], quantity: 1}),
+			LineItem.create({product: products[1], quantity: 1}),
+			LineItem.create({product: products[2], quantity: 0}),
+			LineItem.create({product: products[3], quantity: 0})
+		]
+	})
+];
+
 export default Ember.Service.extend({
-	getOrderById(id) {
-		const orders = this.getOrders();
-		return orders.findBy('id', id);
-	},
-
-	getOrders() {
-		return [
-			{ id: 1, name: 'Tom' },
-			{ id: 2, name: 'Jerry' }
-		];
-	},
-
+	getOrderById(id) { return orders.findBy('id', id); },
+	getOrders() { return orders; },
 	getProducts() { return products; }
 });
