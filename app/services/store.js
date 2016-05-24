b76bbaecaf0acdfd77947cdf1bfff1619b5a4947
Esko Cruzeta <eskocruz@users.noreply.github.com>
2016-05-24 12:02:26 -0700

    define Product records and return with getProducts()

--- app/services/store.js
+++ app/services/store.js
@@ -3,6 +3,13 @@ import LineItem from 'woodland/models/line-item';
 import Order from 'woodland/models/order';
 import Product from 'woodland/models/product';
 
+const products = [
+	Product.create({ title: 'Tent', price: 10, description: 'This tent keeps you off the ground and protected from rain or snow during the night.' }),
+	Product.create({ title: 'Sleeping Bag' price: 5, description: 'Double layer construction pairs with a zipper draft tube and adjustable hood to help seal in warmth and keep cold breezes at bay.' }),
+	Product.create({ title: 'Flashlight', price: 2, description: 'This waterproof, floatable carabiner light features rugged shock-absorbing rubber construction and four super-bright LED lights.' }),
+	Product.create({ title: 'First-Aid Kit', price: 3, description: 'This First Aid Kit is an extensively stocked first aid kit with everything you need for peace of mind.' })
+];
+
 export default Ember.Service.extend({
 	getOrderById(id) {
 		const orders = this.getOrders();
@@ -14,5 +21,7 @@ export default Ember.Service.extend({
 			{ id: 1, name: 'Tom' },
 			{ id: 2, name: 'Jerry' }
 		];
-	}
+	},
+
+	getProducts() { return products; }
 });
