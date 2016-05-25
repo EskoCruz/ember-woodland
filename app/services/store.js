efc683949a939a91f044dfbaa7e760200b9731b0
Esko Cruzeta <eskocruz@users.noreply.github.com>
2016-05-25 15:49:40 -0700

    add "imageUrl" data to "orders" array

--- app/services/store.js
+++ app/services/store.js
@@ -4,10 +4,10 @@ import Order from 'woodland/models/order';
 import Product from 'woodland/models/product';
 
 const products = [
-	Product.create({ title: 'Tent', price: 10, description: 'This tent keeps you off the ground and protected from rain or snow during the night.' }),
-	Product.create({ title: 'Sleeping Bag' price: 5, description: 'Double layer construction pairs with a zipper draft tube and adjustable hood to help seal in warmth and keep cold breezes at bay.' }),
-	Product.create({ title: 'Flashlight', price: 2, description: 'This waterproof, floatable carabiner light features rugged shock-absorbing rubber construction and four super-bright LED lights.' }),
-	Product.create({ title: 'First-Aid Kit', price: 3, description: 'This First Aid Kit is an extensively stocked first aid kit with everything you need for peace of mind.' })
+	Product.create({ title: 'Tent', price: 10, imageUrl: 'assets/images/tent.png', description: 'This tent keeps you off the ground and protected from rain or snow during the night.' }),
+	Product.create({ title: 'Sleeping Bag', price: 5, imageUrl: 'assets/images/bag.png', description: 'Double layer construction pairs with a zipper draft tube and adjustable hood to help seal in warmth and keep cold breezes at bay.' }),
+	Product.create({ title: 'Flashlight', price: 2, imageUrl: 'assets/images/light.png', description: 'This waterproof, floatable carabiner light features rugged shock-absorbing rubber construction and four super-bright LED lights.' }),
+	Product.create({ title: 'First-Aid Kit', price: 3, imageUrl: 'assets/images/kit.png', description: 'This First Aid Kit is an extensively stocked first aid kit with everything you need for peace of mind.' })
 ];
 
 const orders = [
